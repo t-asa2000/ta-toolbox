970629d8e5e863057593958797e1a8b103c46b1a
t-asa2000 <44716771+t-asa2000@users.noreply.github.com>
2024-02-06 10:31:42 +0900

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <v-card class="my-3 mx-auto text-left" width="400" prepend-icon="mdi-home">
+    <template v-slot:title>
+      Vuetify Installed
+    </template>
+
+    <v-card-text>
+      This is content
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
